6f598997dc466724f04b42dfecd9b92304ca05ee
Sunil Simha <sunil.simha95@gmail.com>
2017-12-11 10:52:41 +0530

    Added conditional probabilities

--- MgIIabs/halomodel.py
+++ MgIIabs/halomodel.py
@@ -59,3 +59,70 @@ def s_of_rew(rew,A_w,G0,Rg,ah):
         return brentq(g,0,Rg)
     except(ValueError):
         print("s={:f} cannot be achieved with this model".format(rew))
+
+def kappa_g(M):
+    """
+    This total probability density of finding a halo
+    of mass M. For the classical model, this is a c-spline
+    interpolation of between four different masses
+    logM1 = 10.0, logM2 = 11.33, logM3 = 12.66, logM4 = 14.0
+    (in solar masses) in log-log space
+    Parameters
+    ----------
+    M : float or numpy array
+        Halo mass
+    Returns
+    -------
+    kappa : float or numpy array
+        Probability density at that mass    
+    """
+    import numpy as np
+    from scipy import interpolate
+    #Data from
+    m_array = np.asarray([10.0,11.33,12.66,14.0])
+    kappa_array = np.asarray([-1.721,-0.012,-0.198,-1.763])
+    spline_interp = interpolate.splrep(m_array,kappa_array)
+
+    output_points = 10**interpolate.splev(M,spline_interp)
+    return output_points
+
+def _ds_drew(s,ah,Rg,G0,Aw):
+    """
+    Returns ds/d(rew)
+    """
+    import numpy as np
+    x = lambda s: np.sqrt((Rg**2-s**2)/(ah**2+s**2))
+    y = lambda s: np.sqrt((ah**2+s**2))
+    dsdrew = 1/(2*Aw*G0)/(s*np.arctan(x(s))/y(s)**3 + (1+x(s)**2)*y(s)*s*x(s)/(Rg**2-s**2)/y(Rg)**2)
+    return dsdrew
+
+def p_rew_given_m(rew,M,ah,Rg,G0,Aw):
+    """
+    Returns the conditional probability P(REW|M) for
+    the given value of M and REW.
+
+    Parameters
+    ----------
+    rew : float
+        rest equivalent width of absorption
+    M : float
+        halo mass
+    Aw : float
+        Defined in eq. 6, Tinker & Chen 2008, ApJ
+    G0 : float
+        Normalization factor for halo density.
+    Rg : float
+        Effective gas alo radius
+    ah : float
+        Halo core radius
+    Returns
+    -------
+    p : float
+        P(REW|M) = kappa_g(M)*2s(REW|M)/Rg^2*ds/d(REW)
+    """
+    import numpy as np
+    s = s_of_rew(rew,Aw,G0,Rg,ah)
+    k = kappa_g(M)
+    dsdrew = _ds_drew(s,ah,Rg,G0,Aw)
+    p = k*2*s/Rg**2*dsdrew
+    return p
