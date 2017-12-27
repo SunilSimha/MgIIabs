2f3a70c6c8c111c5c515f5081a4a41203b6302a7
Sunil Simha <sunil.simha95@gmail.com>
2017-12-27 11:21:32 +0530

    credits

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Credits to Prof. Xavier Prochaska for the setup file template.
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, print_function
 #
