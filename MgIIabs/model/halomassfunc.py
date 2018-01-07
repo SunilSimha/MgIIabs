5cdf30ac1602940c2b9144b9ee30654ca1adccd1
Sunil Simha <sunil.simha95@gmail.com>
2018-01-07 22:36:59 +0530

    (code) replaced quad with simps. Factor three speedup

--- MgIIabs/model/halomassfunc.py
+++ MgIIabs/model/halomassfunc.py
@@ -4,6 +4,9 @@ the mass function using Press-Schechter formalism.
 """
 #Note to self: In case of incorrectness of results, look
 #at the constant factors.
+from compos import const, matterps, growthfactor
+import pdb
+const.initializecosmo()
 
 #Need to test for redshift higher than zero.
 
@@ -39,7 +42,7 @@ def window_th(k,R=1):
     #Note to self: Try using astropy units
 
 
-def psvariance(R, growthf=1, W='th',low = 1e-7,high=None,z=None,):
+def psvariance(R, growthf=1,low = -5.0,high=3.0,num=10000):
     """
     Variance of the linear power spectrum. Use the
     power spectrum from COMPOS (Ziang Yen). Numerically
@@ -66,19 +69,20 @@ def psvariance(R, growthf=1, W='th',low = 1e-7,high=None,z=None,):
     error: float
         Error incurred in variance from numerical integration
     """
-    import numpy as np
-    from scipy.integrate import quad
-    from compos import const, matterps, growthfactor
-    import pdb
-    if high is None:
-        high = 20/R
-    const.initializecosmo()
-    #integrand = lambda k: (k*window_th(k,R))**2*gfactor*matterps.normalizedmp(k*const.cosmo['h'])
-    integrand = lambda k: (k*window_th(k,R)*growthf)**2*matterps.normalizedmp(k*const.cosmo['h'])
-    integral = quad(integrand,low,high)
-    variance = integral[0]/(2*np.pi)**2
-    error = integral[1]/(2*np.pi)**2
-    return variance, error
+    from numpy import pi, log, linspace
+    from scipy.integrate import simps
+
+    logk = linspace(low,high,num)
+    k = 10**logk
+    f = (k*window_th(k,R)*growthf)**2*matterps.normalizedmp(k*const.cosmo['h'])*k
+    integral = simps(f,logk)*log(10)/(2*pi)**2
+    #def integrand(log10k):
+    #    k = 10**log10k
+    #    f = (k*window_th(k,R)*growthf)**2*matterps.normalizedmp(k*const.cosmo['h'])*k
+    #    return f
+    #pdb.set_trace()
+    #integral = log(10)*quad(integrand,low,high)[0]/(2*pi)**2
+    return integral
 
 #Include redshift dependence of the parameters.
 def f_of_sigma(sigma,A=0.186,a=1.47,b=2.57,c=1.19,z=0,Delta=200):
@@ -108,12 +112,12 @@ def f_of_sigma(sigma,A=0.186,a=1.47,b=2.57,c=1.19,z=0,Delta=200):
     import numpy as np
     A = A*(1+z)**(-0.14)
     a = a*(1+z)**(-0.06)
-    alpha = np.exp(-(0.075/np.log(Delta/75))**1.2)
+    alpha = 10**(-(0.075/np.log10(Delta/75))**1.2)
     b = b*(1+z)**(-alpha)
     f = A*((sigma/b)**(-a)+1)*np.exp(-c/sigma/sigma)
     return f
 
-def dlogsigma_dr(R,eps=None,**kwargs):
+def dlogsigma_dr(R,eps=None):
     """
     Numerical derivative of the log of standard
     deviation of the power spectrum with the smoothing
@@ -137,7 +141,8 @@ def dlogsigma_dr(R,eps=None,**kwargs):
     if eps is None:
         eps = R*1e-8
     
-    lns = lambda x: 0.5*np.log(psvariance(x,**kwargs)[0])
+    #No redshift dependence oon the slope
+    lns = lambda x: 0.5*np.log(psvariance(x))
     dlnsdR = 0.5*(lns(R+eps)-lns(R-eps))/eps
     return dlnsdR
 
@@ -183,7 +188,7 @@ def dNdM(M,z=0,growthf=None,window='th'):
     R = R.to(Mpc).value
     if growthf is None:
         growthf = growthfactor.growfunc_z(z) 
-    sigma = np.sqrt(psvariance(R,growthf)[0])
+    sigma = np.sqrt(psvariance(R,growthf))
 
-    dNdM = -f_of_sigma(sigma,z=z)*R*dlogsigma_dr(R,growthf=growthf)/3
+    dNdM = -f_of_sigma(sigma,z=z)*R*dlogsigma_dr(R)/3
     return dNdM
